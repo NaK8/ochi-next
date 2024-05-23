7c7d319997a307f25dae6ef557961fdcbaf7c6ab
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2024-05-24 03:53:29 +0500

    Layout Fix

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -3,8 +3,13 @@ import MarqueSlide from "./MarqueSlide";
 
 function Marquee() {
   return (
-    <section className="w-full overflow-hidden rounded-t-3xl bg-[#004d43] py-12">
-      <div className="flex whitespace-nowrap border-y border-zinc-100 py-4 text-white">
+    <section
+      data-scroll
+      data-scroll-sections
+      data-scroll-speed="-0.001"
+      className="w-full rounded-t-3xl bg-[#004d43] py-12"
+    >
+      <div className="flex overflow-hidden whitespace-nowrap border-y border-zinc-100 py-4 text-white">
         <MarqueSlide>We are ochi &nbsp;</MarqueSlide>
         <MarqueSlide>We are ochi &nbsp;</MarqueSlide>
         <MarqueSlide>We are ochi &nbsp;</MarqueSlide>
