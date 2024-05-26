ea69a29c405502f2cbab2cf6e3017ca47cd05324
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2024-05-27 03:35:53 +0500

    fix console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
       const locomotiveScroll = new LocomotiveScroll({
         smooth: true,
       });
+      return locomotiveScroll;
     })();
   }, []);
   return (
